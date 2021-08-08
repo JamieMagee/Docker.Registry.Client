91e002e39a3d999fe234bbddf377bf8a3f33f94a
Jamie Magee <jamie.magee@gmail.com>
2021-08-07 20:03:13 -0700

    Add integration tests

---
+++ Tests/Docker.Registry.Client.Tests.Integration/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Docker.Registry.Client.Tests.Integration
+{
+    public static class Constants
+    {
+        public const string DockerHubUsernameEnvironmentVariable = "DOCKER_HUB_USERNAME";
+
+        public const string DockerHubTokenEnvironmentVariable = "DOCKER_HUB_TOKEN";
+    }
+}
---
+++ Tests/Docker.Registry.Client.Tests.Integration/DockerHub/DockerHubBaseTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Docker.Registry.Client.Tests.Integration.DockerHub
+{
+    using System;
+    using Authentication;
+    using Registry;
+
+    public class DockerHubBaseTest
+    {
+        protected readonly IRegistryClient client;
+
+        public DockerHubBaseTest()
+        {
+            var authentication = new PasswordOAuthAuthenticationProvider(
+                Environment.GetEnvironmentVariable(Constants.DockerHubUsernameEnvironmentVariable),
+                Environment.GetEnvironmentVariable(Constants.DockerHubTokenEnvironmentVariable));
+            this.client = new RegistryClientConfiguration("registry-1.docker.io").CreateClient(authentication);
+        }
+    }
+}
